d14315d3f98249b4a0db474e0daf4f7978c1776c
Moreto23 <93287905+Moreto23@users.noreply.github.com>
2025-11-27 16:46:28 -0500

    Update planes.service.ts

--- gymfrontend/src/app/services/planes.service.ts
+++ gymfrontend/src/app/services/planes.service.ts
@@ -2,13 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { toHttpParams } from './http.util';
 
+const API = 'https://backendgym-1-id69.onrender.com/api/planes';
+
 @Injectable({ providedIn: 'root' })
 export class PlanesService {
-  private readonly base = 'https://backendgym-1-id69.onrender.com/api/planes';
+
+  private readonly base = API;
+
   constructor(private http: HttpClient) {}
-  listar() { return this.http.get<any[]>(`${this.base}`); }
-  obtener(id: number) { return this.http.get<any>(`${this.base}/${id}`); }
+
+  listar() {
+    return this.http.get<any[]>(this.base);
+  }
+
+  obtener(id: number) {
+    return this.http.get<any>(`${this.base}/${id}`);
+  }
+
   suscribirse(id: number, usuarioId: number, monto?: number) {
-    return this.http.post<any>(`${this.base}/${id}/suscribirse`, null, { params: toHttpParams({ usuarioId, monto }) });
+    return this.http.post<any>(`${this.base}/${id}/suscribirse`, null, {
+      params: toHttpParams({ usuarioId, monto })
+    });
   }
 }
