ca798c3d433360e3098bd21260fd98ec27c1d753
Moreto23 <93287905+Moreto23@users.noreply.github.com>
2025-11-27 10:34:58 -0500

    Update usuario.service.ts

--- gymfrontend/src/app/services/usuario.service.ts
+++ gymfrontend/src/app/services/usuario.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class UsuarioService {
-  private readonly BASE = 'https://backendgym-1-id69.onrender.com/api/usuarios';
+  private readonly base = 'https://backendgym-1-id69.onrender.com/api/usuarios';
   constructor(private http: HttpClient) {}
   obtener(id: number) { return this.http.get<any>(`${this.base}/${id}`); }
   actualizar(id: number, data: Partial<{ telefono: string; direccion: string }>) { return this.http.patch<any>(`${this.base}/${id}`, data); }
