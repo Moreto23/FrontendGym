5e283d3e83bf7ee48e96545c1016237434635a16
Moreto23 <93287905+Moreto23@users.noreply.github.com>
2025-11-27 16:45:49 -0500

    Update dashboard.service.ts

--- gymfrontend/src/app/services/dashboard.service.ts
+++ gymfrontend/src/app/services/dashboard.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const API_BASE = 'https://backendgym-1-id69.onrender.com/api/dashboard';
+
 @Injectable({ providedIn: 'root' })
 export class DashboardService {
-  private base = '/api/dashboard';
+
+  private base = API_BASE;
+
   constructor(private http: HttpClient) {}
-  resumen(usuarioId: number) { return this.http.get<any>(`${this.base}/${usuarioId}`); }
+
+  resumen(usuarioId: number) {
+    return this.http.get<any>(`${this.base}/${usuarioId}`);
+  }
 }
